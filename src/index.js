6d1a6e1b4b5504e5ac99a770390d72fa01f82e4a
Venijeya <143690071+Venijeya@users.noreply.github.com>
2026-05-02 12:33:52 +0530

    add search console verification

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import ReactGA from 'react-ga4';
+
+ReactGA.initialize('G-WWK7ZPQVG2'); // இங்க உன் ID போடு
+ReactGA.send({ hitType: "pageview", page: window.location.pathname });
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
